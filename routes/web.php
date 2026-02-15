2fef65cca5a948d775b5f2aee5286933c456b1f0
Paulo Ricardo <aleixopaulo22@gmail.com>
2026-02-15 18:33:56 -0300

    Redirect homepage to products instead of categories

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect()->route('products.categories');
+    return redirect()->route('products.index');
 });
 
 
